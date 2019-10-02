44f0c77eed44e0252a8fb2c37e66940d029e5e87
Administrator <Administrator@SHARE10S-PC>
2019-10-02 12:03:02 +0700

    Add project files.

---
+++ CongTruNhanChia/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CongTruNhanChia
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public int Tong(int n)
+        {
+            int tong = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                tong += i;
+            }
+
+            return tong;
+        }
+
+        public int TongChan(int n)
+        {
+            int tongChan = 0;
+            for (int i = 0; i <= n; i += 2)
+            {
+                tongChan += i;
+            }
+            return tongChan;
+        }
+
+        public int TongLe(int n)
+        {
+            int tongLe = 0;
+            for (int i = 1; i <= n; i += 2)
+            {
+                tongLe += i;
+            }
+            return tongLe;
+        }
+
+        public int chon()
+        {
+            if (rdTong.IsChecked == true)
+                return 1;
+            else if (rdTongChan.IsChecked == true)
+                return 2;
+            else if (rdTongLe.IsChecked == true)
+                return 3;
+
+            return 0;
+        }
+
+        public void disPlay(int chon)
+        {
+
+
+            int n = int.Parse(txbNhap.Text);
+            int tong, tongLe, tongChan;
+            tong = Tong(n);
+            tongLe = TongLe(n);
+            tongChan = TongChan(n);
+
+            switch (chon)
+            {
+                case 1:
+                    txbKetQua.Text = tong.ToString();
+                    break;
+
+            }
+        }
+    }
+}
